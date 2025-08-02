d6b158301faf8c69646421417ab07b1f7cd1352a
지승 <92665200+Ryuider-JS@users.noreply.github.com>
2025-08-02 16:10:31 +0900

    Add files via upload

--- jeju_app.py
+++ jeju_app.py
@@ -6,7 +6,7 @@ from folium.plugins import MarkerCluster
 
 st.title("제주도에서 가장 핫한 곳은?")
 
-df = pd.read_csv("./map/jeju_place.csv")
+df = pd.read_csv("./jeju_place.csv")
 
 m = folium.Map([sum(df["위도"])/len(df["위도"]), sum(df["경도"])/len(df["경도"])], zoom_start=9)
 
